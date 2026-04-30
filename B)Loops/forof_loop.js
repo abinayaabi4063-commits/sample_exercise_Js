6faff3261e72aafb834f24a885c0f2fcb625bae7
abinayaabi4063 <abinayaabi4063@gmail.com>
2026-04-30 20:58:50 +0530

    add

---
+++ B)Loops/forof_loop.js
@@ -0,0 +1,19 @@
+let numbers = [10,20,30];
+
+for (let item of numbers) {
+    // console.log(item);                // itterate and returns to values -> 10 20 30
+}
+
+
+
+
+
+
+let text = "hello world"
+
+
+for(let item of text){
+  
+    console.log(item)            
+    
+}
