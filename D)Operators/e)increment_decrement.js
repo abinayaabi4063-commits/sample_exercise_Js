74f7a1bb6ce69969feb115335f11aae73387f905
abinayaabi4063 <abinayaabi4063@gmail.com>
2026-04-30 20:32:06 +0530

    add

---
+++ D)Operators/e)increment_decrement.js
@@ -0,0 +1,13 @@
+/**
+ * pre increment  => ++variable
+ * post increment => varable++
+ * pre decrement  => --variable
+ * post decrement => variable--
+ **/
+
+let i = 3;
+
+console.log("i====>",i++)    // 
+console.log("i=====>",++i)   // 
+console.log("i====>",i--)    //
+console.log("i====>",--i)   // 
