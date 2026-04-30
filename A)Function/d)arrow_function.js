3dae0581639b0f42e6aa8b66f4412fce4b0a6d87
abinayaabi4063 <abinayaabi4063@gmail.com>
2026-04-30 21:24:54 +0530

    add

---
+++ A)Function/d)arrow_function.js
@@ -0,0 +1,16 @@
+/* Normal function  */
+
+
+function welcomeMessage(){
+    console.log("welcome to Javascript world")
+}
+
+welcomeMessage()
+
+const expression = function (){
+    console.log("welcome to Javascript world")
+}
+expression()
+
+
+
