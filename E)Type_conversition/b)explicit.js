87b1d10f025a2f63aac6a6a6b5ce18eda441a18e
abinayaabi4063 <abinayaabi4063@gmail.com>
2026-04-30 20:43:21 +0530

    add

---
+++ E)Type_conversition/b)explicit.js
@@ -0,0 +1,39 @@
+/* Example 1 : String to Number */
+
+
+var firstNumber = "100"
+var secondNumber = "500"
+
+firstNumber = Number(firstNumber)
+secondNumber = Number(secondNumber)
+
+var result  = firstNumber + secondNumber;    // 600
+
+
+
+
+
+
+/* Example 2 : Number to String */
+
+
+var firstNumber = 100
+var secondNumber = 500
+
+firstNumber = String(firstNumber)
+secondNumber = String(secondNumber)
+
+var result  = firstNumber + secondNumber;    // 100500
+
+//console.log(result)
+
+
+
+
+/* Example 3 : Boolean conversation */
+
+let productAvailable = 1
+var result = Boolean(productAvailable)
+
+
+console.log(result)
