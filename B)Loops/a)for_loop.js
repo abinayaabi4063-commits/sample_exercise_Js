725eca5ad126c150b6ddb580dfdf7956bb359e6a
abinayaabi4063 <abinayaabi4063@gmail.com>
2026-04-30 20:55:13 +0530

    add

---
+++ B)Loops/a)for_loop.js
@@ -0,0 +1,23 @@
+/* Example 1 */
+
+for(let i = 1; i<=10; i++){
+
+  //console.log(`${i}," *2 = " , ${i*2}`)
+}
+
+
+
+
+
+
+
+
+/* Example 2 */
+
+let fruits = ["apple","orange","grapes","kiwi"]
+
+for(let i=0; i<fruits.length; i++){
+  
+  console.log("fuits===>",fruits[i])
+
+}
