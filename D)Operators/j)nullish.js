565d652f9b6ca3d125b3eef68c2a073469dfd2af
abinayaabi4063 <abinayaabi4063@gmail.com>
2026-04-30 20:37:00 +0530

    add

---
+++ D)Operators/j)nullish.js
@@ -0,0 +1,5 @@
+var defaultCm = null ?? "Vijay";
+
+console.log("candidate Name ====>",defaultCm)
+console.log("candidate Name length====>",defaultCm.length)
+console.log("candidate Name uppercase====>",defaultCm.toUpperCase())
