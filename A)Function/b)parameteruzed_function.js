c8b6dc664d4046b37e46543e4605d5f8c8029770
abinayaabi4063 <abinayaabi4063@gmail.com>
2026-04-30 21:22:17 +0530

    add

---
+++ A)Function/b)parameteruzed_function.js
@@ -0,0 +1,16 @@
+console.log("Program Start....")
+
+function addition(){
+    let a = 10;
+    let b = 5;
+    let c = a + b;
+    
+    return a
+}
+
+let result = addition()
+
+console.log("result====>",result)
+
+console.log("Program End")
+
