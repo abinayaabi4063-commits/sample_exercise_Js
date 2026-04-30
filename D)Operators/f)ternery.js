92e3a48d1d4509cf84bbae64c3bcc0ed07997c4f
abinayaabi4063 <abinayaabi4063@gmail.com>
2026-04-30 20:33:14 +0530

    add

---
+++ D)Operators/f)ternery.js
@@ -0,0 +1,12 @@
+   /*** 
+     *   Ternary Operator - similar as if else condition.
+     *     
+     *    ?  True Block    :  False Block
+     **/
+    
+    
+    let age = 53;
+
+    let result = (age >= 18) ? "Adult" : "Minor";
+    
+    console.log(result);
