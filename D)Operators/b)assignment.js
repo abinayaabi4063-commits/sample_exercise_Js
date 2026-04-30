f05ea1fa522dea98bd0e117b203489a4cbf24113
abinayaabi4063 <abinayaabi4063@gmail.com>
2026-04-30 20:25:53 +0530

    add

---
+++ D)Operators/b)assignment.js
@@ -0,0 +1,19 @@
+let x = 10;
+console.log("x value is ====>",x)
+
+
+/* addition assignment */
+x += 25  
+console.log("addition assignment====>",x)          // 35
+
+/* subraction assignment */
+x -= 10
+console.log("subraction assignment====>",x)      // 25     
+
+/* Multiplication assignment */
+x *= 2
+console.log("multiplication assignment====>",x)   // 50
+
+/* division assignment */
+x /= 2
+console.log("multiplication assignment====>",x)  // 25 
