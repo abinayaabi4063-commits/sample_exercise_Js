2dd05be393b02dc483d6be57aa5543af6858f09f
abinayaabi4063 <abinayaabi4063@gmail.com>
2026-05-01 19:48:26 +0530

    add

--- A)Variables/a)functionsope.js
+++ A)Variables/a)functionsope.js
@@ -1,17 +1,7 @@
-/**
- *  var allow 
- *         -> function (or) global scope
- *         -> allow re-declare
- *         -> allow re-assign
- */
-
-
-var bottle = "oil"                           // declare
-var bottle = "water"                         // re-declare
+var bottle = "oil"                         
+var bottle = "water"                        
 var bottle = "juice"
 var bottle = "curd"
-
-  bottle = "chilli powder"                // re-assign
+  bottle = "chilli powder"              
   bottle = "mouth-wash"
-
 console.log("bottle====>",bottle)
