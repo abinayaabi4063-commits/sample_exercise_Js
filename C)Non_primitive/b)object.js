8a910981caf5717b1604f06b9bc3317322605614
abinayaabi4063 <abinayaabi4063@gmail.com>
2026-05-01 21:28:39 +0530

    add

--- C)Non_primitive/b)object.js
+++ C)Non_primitive/b)object.js
@@ -1,39 +1,7 @@
-/* Example 1 - Simple object creation */
-
-  let student = {
-        "name" : "saravanan",
-        "age"  : 5,
-        "school":"dav matriculation",
-        "location":"chennai"
-  }
-
-//   console.log("student Name ==>",student.name)
-//   console.log("student age===>",student.age)
-//   console.log("student school===>",student['school'])
-
-
-
-
-
-
-/* Example 2*/
-
-const employee = {
-    name : "saravanan",
-    age : "28",
-    location : "chennai",
-    isWorking : true,
-    salary : 52454.25,
-    skillset : ["Java","python","react","angular"],
-    yearofExp : {
-        "2022-2023" : "Comodo security solutions",
-        "2025-2026" : "Tata consultancy services"
-
-    }
-}
-
-console.log("employeee Name ======>",employee.name)   
-console.log("employeee skillset ======>",employee.skillset)       
-console.log("employeee main skill ======>",employee.skillset[2])
-console.log("employee total experience details====>",employee.yearofExp)
-console.log("current working status====>",employee.yearofExp["2025-2026"])
+let student = {
+  name: "gokul",
+  age: 22,
+  isPass: true
+};
+console.log(student.name);
+console.log(student.age);
