0a6f166dddaa8eacd5d2960f84604d12adf502ed
abinayaabi4063 <abinayaabi4063@gmail.com>
2026-05-01 21:22:10 +0530

    add

--- C)Non_primitive/a)arrays.js
+++ C)Non_primitive/a)arrays.js
@@ -1,19 +1,4 @@
-/**
- *  Array => an array is a group of related data item.
- */
-
-let fruits = ["apple","ornage","grapes","pineapple","lichi"]
-let salaryBasic = [5000,6000,7000,9000,8000]
-
-
-
-
-/**
- *  List => A list is a group of un-related data item.
- */
-
-let personDetails = ["saravanan",27,"MCA",52000.25,]
-
-
-console.log("personDetails====>",personDetails)
-console.log("person qualification ====>",personDetails[2])
+let fruits = ["Apple", "Mango", "Banana"];
+console.log(fruits);
+console.log(fruits[0]);
+console.log(fruits[2]);
