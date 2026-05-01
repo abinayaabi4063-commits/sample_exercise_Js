1df75c68673893c2644c7d96bf71c323c0cfef67
abinayaabi4063 <abinayaabi4063@gmail.com>
2026-05-01 20:17:59 +0530

    add

--- B)Primitive_datatype/a)string_type.js
+++ B)Primitive_datatype/a)string_type.js
@@ -1,21 +1,3 @@
-// String
-let candidateFirstName = "saravanan";
-let candidateLastName = "Durai";
-let initial = 'D'
-
-// console.log("Initial===>", initial)
-// console.log( candidateFirstName +' '+ candidateLastName)
-
-
-
-
-let firstNumber = "100";
-let secondNumber = "200";
-
-//console.log(firstNumber + secondNumber)
-
-
- let productAvailabe = "true";
-
- let data = "null";
- let product = "undefined";
+let name = "abi"; 
+let city = "kallai";
+console.log(name);
