527cd387e85e224d4c9f30178c223be36418d949
abinayaabi4063 <abinayaabi4063@gmail.com>
2026-05-01 16:52:18 +0530

    add

---
+++ A)Function_currying/b)currying.js
@@ -0,0 +1,36 @@
+function student(mark){
+
+    return function() {
+        let grade = null
+        if(mark >=90)
+            grade = "A"
+        else if(mark >= 75)
+            grade = "B"
+        else if(mark >= 50)
+            grade = "C"
+        else
+            grade = "F"
+
+        return function(){
+            let result;
+            switch(grade){
+                case "A" :
+                    result = "Excellent Performer"
+                    break;
+                case "B":
+                    result = "Good Performer"
+                    break
+                case "C":
+                    result = "Average Performer"
+                    break
+                default:
+                    result = "Fail"
+            }
+            return result
+        }
+    }
+
+}
+
+let result = student(75)()()
+console.log("result====>",result)
