5eeba6fe9588b096dfc83a1239641acb8de973ca
abinayaabi4063 <abinayaabi4063@gmail.com>
2026-05-01 17:03:32 +0530

    add

---
+++ C)String_function/b)search_function.js
@@ -0,0 +1,7 @@
+let str = "Hello welcome to Javascript"
+let findText = "welcome"
+
+let isAvailablestatus = str.includes(findText)             // true or false
+let index = str.indexOf(findText)                          // 6 or -1
+
+console.log("index====>",index)
