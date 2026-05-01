75067f2cce1fd6f9dd7b58bf1c98396964b50915
abinayaabi4063 <abinayaabi4063@gmail.com>
2026-05-01 19:53:14 +0530

    add

--- A)Variables/c)constans_variable.js
+++ A)Variables/c)constans_variable.js
@@ -1,20 +1,3 @@
-/**
- *  const -> block scope
- *        -> stands for constant
- *        -> restricted re-declare
- *        -> restricted re-assign
- */
-
-
-
-const bottle = "oil";
-bottle = "water";
-
-const plasticCover = "chilliPowder";
-
-
-
-// Project - vise
-
-const dbUrl = "mysql://localhost:3306/mydb";
-const paymentUrl = "razorpay:9123@abl.com"
+const name = "abi";
+console.log(name);
+name = "vini"; 
