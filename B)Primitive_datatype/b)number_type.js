1fa7995ae324a3c8d5a7290aa3c49ca5c9ade462
abinayaabi4063 <abinayaabi4063@gmail.com>
2026-05-01 20:24:15 +0530

    add

--- B)Primitive_datatype/b)number_type.js
+++ B)Primitive_datatype/b)number_type.js
@@ -1,15 +1,4 @@
-/***
- *    Number Data type => it's comes to
- *                     => whole number or decimal  (possitve and negative)
- *                     
- */
-
-
-let principalAmount = 1000;      // whole numbber
-let intrestPerncentage = 7.5;    // Decimal number
-
-let simpleInterest  = principalAmount * intrestPerncentage / 100;
-
-let currentBalance = -500;
-
-console.log("currentBalance ====>", currentBalance)
+let age = 22;
+let price = 99.99;
+console.log(age + 8); 
+console.log(age);
