0a37d2e6ddd4b43aae05063eca30237d30904abf
abinayaabi4063 <abinayaabi4063@gmail.com>
2026-05-02 10:59:48 +0530

    add

--- D)Operators/d)logical.js
+++ D)Operators/d)logical.js
@@ -4,26 +4,11 @@
  *     (||) OR  -  [any condition true] = true otherwise false
  *     (!)  Not -  [Negativity check]
  */
-
-
-var x = 100
-var y = 100
-
-var a = 500
-var b = 600
-/* Example : AND */
-console.log( (x==y) && (a==b) )                  //  -->  false
-
-/*  Example : OR */
-
-console.log( (x==y) || (a==b) )                 // --> true
-
-/* Example : Not (!) */
-
-var productAvailable = true;
-var product = "apple"
-var productprice = 100;
-
-var productOffer = !productAvailable ? "stock empty" : productprice * 5 /100;
-
-console.log("productOffer===>",productOffer)
+let vayasu = 25;
+let licenseIrukka = true;
+console.log(vayasu > 18 && licenseIrukka);
+let cash = false;
+let card = true;
+console.log(cash || card); 
+let rain = false;
+console.log(!rain);
