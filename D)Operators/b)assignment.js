1f5d589057b5e6ea6077c10fd34ddd106ed178d7
abinayaabi4063 <abinayaabi4063@gmail.com>
2026-05-02 10:24:51 +0530

    add

--- D)Operators/b)assignment.js
+++ D)Operators/b)assignment.js
@@ -1,19 +1,5 @@
-let x = 10;
-console.log("x value is ====>",x)
-
-
-/* addition assignment */
-x += 25  
-console.log("addition assignment====>",x)          // 35
-
-/* subraction assignment */
-x -= 10
-console.log("subraction assignment====>",x)      // 25     
-
-/* Multiplication assignment */
-x *= 2
-console.log("multiplication assignment====>",x)   // 50
-
-/* division assignment */
-x /= 2
-console.log("multiplication assignment====>",x)  // 25 
+let x = 5; // = na x ku 5 ah kuduthom
+x += 3;    // x = x + 3. Ipo x = 8 aagidum
+x -= 2;    // x = x - 2. Ipo x = 6 aagidum
+x *= 2;    // x = x * 2. Ipo x = 12 aagidum
+console.log(x); // 12
