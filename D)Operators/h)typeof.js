648111c581bc984e56f506ca8964b9569e007b6b
abinayaabi4063 <abinayaabi4063@gmail.com>
2026-05-02 11:35:59 +0530

    add

--- D)Operators/h)typeof.js
+++ D)Operators/h)typeof.js
@@ -3,13 +3,9 @@ let intrest = "250";
 let employeeName = "saravanan";
 let loanStatus = true;
 let result = 0;
-
-
 console.log("principal Amount====>",typeof principalAmount)
 console.log("employee Name   ====>",typeof employeeName)
 console.log("employee status   ====>",typeof isEmployeestatus)
-
-
 if(typeof principalAmount == "number" && typeof intrest == "number"){
     result = principalAmount + intrest
 }
