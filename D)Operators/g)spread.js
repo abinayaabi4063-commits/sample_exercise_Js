6823c1558d551cd3f882f0564997e08fe120923f
abinayaabi4063 <abinayaabi4063@gmail.com>
2026-05-02 11:27:00 +0530

    add

--- D)Operators/g)spread.js
+++ D)Operators/g)spread.js
@@ -1,50 +1,6 @@
-/**
- *    shallow copy vs deepcopy
- *    
- *   Spread operator => it is helps to copying an array element in deepcopy
- * 
- */
-
- 
-/** 
- *  Example  1 - Shallow copy 
- * 
- * 
- * */ 
-
-var fruits = ["apple","orange","grapes","banana"]              // 200tkl856
-var newFruits = fruits   /* Shallow copy */                    // &200tkl856
-
-
-newFruits.push("pineapple")
-newFruits.push("papaya")
-
-
-//  console.log("Fruits ======>",fruits)
-//  console.log("New Fruits=====>",newFruits)
-
-
-
-
-
-
-
-
-
-
-
-
-/* Example2 - Spread operator  */ 
-
-
-
-
 var fruits = ["apple","orange","grapes","banana"]             
-var newFruits = [...fruits]                             /* Deep copy */
- 
+var newFruits = [...fruits]                             
 newFruits.push("gova")
 newFruits.push("lichi")
-
-
 console.log("fruits====>",fruits)
 console.log("new Fruits====>",newFruits);
