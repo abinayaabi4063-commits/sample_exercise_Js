3c20212157bc8ccd415409fc5f3e9d55487f616c
abinayaabi4063 <abinayaabi4063@gmail.com>
2026-05-03 20:18:06 +0530

    add

--- A)Function/b)parameteruzed_function.js
+++ A)Function/b)parameteruzed_function.js
@@ -1,16 +1,5 @@
-console.log("Program Start....")
-
-function addition(){
-    let a = 10;
-    let b = 5;
-    let c = a + b;
-    
-    return a
+function greet(name) {    // name = parameter
+  console.log("Hello " + name);
 }
-
-let result = addition()
-
-console.log("result====>",result)
-
-console.log("Program End")
-
+greet("Abinaya");
+greet("vini"); // vini = argument
