e24d768e9d47af2c480b23855b8c1b88b2433bf3
abinayaabi4063 <abinayaabi4063@gmail.com>
2026-05-03 19:02:46 +0530

    add

--- A)Function/a)function_declaration.js
+++ A)Function/a)function_declaration.js
@@ -1,16 +1,4 @@
-console.log("Program Start....")
-
-function addition(){
-    let a = 10;
-    let b = 5;
-    let c = a + b;
-    
-    return a
+function greet() {
+  console.log("Hello abinaya");
 }
-
-let result = addition()
-
-console.log("result====>",result)
-
-console.log("Program End")
-
+greet(); 
