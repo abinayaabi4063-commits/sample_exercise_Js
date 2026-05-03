97fd6c8b93426dcfb73208308b8655f3b2fdfd33
abinayaabi4063 <abinayaabi4063@gmail.com>
2026-05-03 18:17:17 +0530

    add

--- B)Loops/b)forin_loop.js
+++ B)Loops/b)forin_loop.js
@@ -1,30 +1,8 @@
-/* Example 1 :  String itteration using in operator */
-
-let text = "Hello welcome to Javascript world"
-
-for(let item in text){
-     // console.log("item====>",text.charAt(item))
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* Example 2 : Array itteration */
-
-let fruits = ["apple","orage","grapes"]
-
-for(let item in fruits){
-    console.log("item===>",fruits.at(item) )
+let employee = {
+  name: "Abinaya",
+  role: "HR",
+  company: "AG Company"
+};
+for(let key in employee) {
+  console.log(key + " : " + employee[key]);
 }
