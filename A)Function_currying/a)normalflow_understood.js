ded1a4121c57d1c8c4c234db4992fbf58a1e6071
abinayaabi4063 <abinayaabi4063@gmail.com>
2026-05-04 20:34:13 +0530

    add

--- A)Function_currying/a)normalflow_understood.js
+++ A)Function_currying/a)normalflow_understood.js
@@ -1,16 +1,10 @@
 function employeeSalaryCompute(basic,hra,pa){
-
     return function(pf){
         let salary = (basic + hra + pa) - pf
-
         return function(tax){
             let netSalary = salary - (salary * tax / 100)
             return netSalary;
         }
-       
     }
-
-    
 }
-
 let netSalary = employeeSalaryCompute(5000,2500,50000)(3600)(8)
