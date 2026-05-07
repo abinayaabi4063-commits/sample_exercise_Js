17a3c637d64668b40278695a8e4e4fb61348b9b3
abinayaabi4063 <abinayaabi4063@gmail.com>
2026-05-07 23:18:26 +0530

    add

--- C)String_function/f)chain_function.js
+++ C)String_function/f)chain_function.js
@@ -1,7 +1,6 @@
-let string = "Saravanan durai is a software developer"
-
-let newStr = string.charAt(10).toUpperCase() + " " + string.slice(0,9)     
-
-let reverseStr = newStr.split("").reverse().join("")
-
-console.log(reverseStr)
+let name = "  abinaya varun ";
+let result = name
+  .trim()           // "abinaya ravi"
+  .toUpperCase()    // "ABINAYA RAVI"
+  .replace(" ", "_"); // "ABINAYA_RAVI"
+console.log(result); // ABINAYA_RAVI
