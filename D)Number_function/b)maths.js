ca82bbe75f9605fb344a6bebda7f17e2fc54de55
abinayaabi4063 <abinayaabi4063@gmail.com>
2026-05-07 23:23:17 +0530

    add

--- D)Number_function/b)maths.js
+++ D)Number_function/b)maths.js
@@ -1,4 +1,4 @@
-let num = 4.2
-let round = Math.round(num)  //4.6
-
-console.log(round)
+console.log(Math.round(4.6));   // 5 - nearest integer ku round
+console.log(Math.floor(4.9));   // 4 - kela irangum
+console.log(Math.ceil(4.1));    // 5 - mela yerum
+console.log(Math.trunc(4.9));   // 4 - decimal ah cut pannum
