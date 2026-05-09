5917e8c274a56880fe7cb0734589221c2729465a
abinayaabi4063 <abinayaabi4063@gmail.com>
2026-05-09 20:26:22 +0530

    add

---
+++ A_Arrayfunction/e)convert_string.js
@@ -0,0 +1,4 @@
+let fruits = ["apple","orange","grapes","pineapple","mango"]
+let arrTostring = fruits.toString()
+let removeSplchar = fruits.join("*")
+console.log("removeSplchar====>", removeSplchar)
