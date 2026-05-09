39cadac4e18b1cb17cab5a9dba9f00344e11a8ff
abinayaabi4063 <abinayaabi4063@gmail.com>
2026-05-09 21:52:24 +0530

    add

---
+++ A_Selecting_element/a)index.js
@@ -0,0 +1,5 @@
+let fruits = ["apple", "banana", "mango"];
+console.log(fruits[0]); // apple - first item
+console.log(fruits[1]); // banana - second item
+console.log(fruits[2]); // mango - third item
+console.log(fruits.length); // 3 - total count
