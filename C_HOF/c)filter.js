ec124e522ed2322be489bb6d06a4c8f6ffd32482
abinayaabi4063 <abinayaabi4063@gmail.com>
2026-05-09 20:46:44 +0530

    add

---
+++ C_HOF/c)filter.js
@@ -0,0 +1,4 @@
+let nums = [5, 10, 15, 20];
+let bigNums = nums.filter(num => num > 10); // 10 ku mela irukura mathiri mattum
+console.log(bigNums); // [15, 20]
+console.log(nums);    // [5, 10, 15, 20] - original safe
