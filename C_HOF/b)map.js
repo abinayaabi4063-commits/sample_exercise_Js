b5aa57f95d7515442d9e73901d2608055e0732d9
abinayaabi4063 <abinayaabi4063@gmail.com>
2026-05-09 20:44:50 +0530

    add

---
+++ C_HOF/b)map.js
@@ -0,0 +1,6 @@
+let nums = [1, 2, 3];
+let doubled = nums.map(function(num) {
+  return num * 2; // ella num ah 2 la multiply pannu
+});
+console.log(doubled); // [2, 4, 6]
+console.log(nums);    // [1, 2, 3] - original change aagadhu
