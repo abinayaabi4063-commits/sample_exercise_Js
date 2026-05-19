87ed93860a94869098ba29cdd0bddbe4ba964cb5
abinayaabi4063 <abinayaabi4063@gmail.com>
2026-05-19 21:03:32 +0530

    add

---
+++ A_Selecting_element/c)index.js
@@ -0,0 +1,7 @@
+alert("Welcome");
+function changeText() {
+  document.getElementById("heading").innerText = "Button work aagudhu!";
+  document.getElementById("message").innerText = "JS dha pa gethu 🔥";
+  document.getElementById("message").style.color = "red";
+}
+console.log("Ennada site open pannita pola");
